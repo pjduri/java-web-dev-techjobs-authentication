38a39e2873d0a1860ad17ce13401c2a1dc46740b
Your Name <pjduri@gmail.com>
2022-11-10 15:38:15 -0600

    Built User class

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/data/User.java
@@ -0,0 +1,38 @@
+package org.launchcode.javawebdevtechjobsauthentication.models.data;
+
+import org.launchcode.javawebdevtechjobsauthentication.models.AbstractEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class User extends AbstractEntity {
+
+    @NotNull
+    @Size(min = 3, max = 55)
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public User () {}
+
+    public User(String username, String password) {
+        super();
+        this.username = username;
+        this.pwHash = encoder.encode(password);
+    }
+
+    public String getName() {
+        return username;
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
+
+}
