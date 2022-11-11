6e16a49c4ccc82faf17e4b0a4f6b5e760d83d4e8
Your Name <pjduri@gmail.com>
2022-11-10 21:24:14 -0600

    finished with Jayde during review - the issues we had were that the size min for username needed to be 6 and the navbar (page-header fragment) wasn't on the form templates in Jayde's solution

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/dto/LoginFormDTO.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/dto/LoginFormDTO.java
@@ -1,5 +1,6 @@
 package org.launchcode.javawebdevtechjobsauthentication.models.dto;
 
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -8,7 +9,7 @@ public class LoginFormDTO {
 
     @NotNull
     @NotBlank
-    @Size(min = 3, max = 20, message = "Invalid username. Must be between 3 and 20 characters.")
+    @Size(min = 6, max = 20, message = "Invalid username. Must be between 6 and 20 characters.")
     private String username;
 
     @NotNull
